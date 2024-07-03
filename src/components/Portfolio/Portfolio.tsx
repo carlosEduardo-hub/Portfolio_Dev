59da821e6f4b683e8a8ac2b36c12f23bfb80c2c1
carlosEduardo-hub <carloseduardo010616@gmail.com>
2024-07-03 12:51:41 -0300

    feat:add DevBurguer project

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -370,6 +370,60 @@ export function Portfolio() {
             </footer>
           </div>
         </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/carlosEduardo-hub/Projeto-DevBurguer"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={githubIcon} alt="Visitar site" />
+                </a>
+                <a
+                  href="https://dev-burguer-kappa.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Cardápio de Hamburgueria - Delivery Online</h3>
+              <p>
+                Este projeto é um cardápio online de hamburgueria desenvolvido
+                em HTML, CSS, TailwindCSS e JavaScript. Ele permite que os
+                clientes naveguem pelos itens do menu, adicionem-os ao carrinho
+                e finalizem o pedido, o qual é enviado diretamente para o
+                WhatsApp.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Html</li>
+                <li>TailwindCss</li>
+                <li>JavaScript</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
       </div>
     </Container>
   );
